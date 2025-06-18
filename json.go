6d6e7a88964d644f6ab76f4c3e67630df52083e8
sagar4tech <sagargupta4you@gmail.com>
2025-06-18 12:26:20 +0530

    security check with Gosec step added in the tests job

--- json.go
+++ json.go
@@ -30,5 +30,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	if _, err := w.Write(dat); err != nil {
+		// Optionally log or handle the error
+		// Example:
+		log.Printf("failed to write response: %v", err)
+	}
 }
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -87,10 +88,10 @@ func main() {
 
 	v1Router.Get("/healthz", handlerReadiness)
 
-	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second, // Add this line
 	}
 
 	log.Printf("Serving on port: %s\n", port)
